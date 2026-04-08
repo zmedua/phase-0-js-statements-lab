5d9f654f4cbc050b167b5a8e1d4c12d76ce316c9
Zane <zimedua@gmail.com>
2026-04-08 12:47:28 -0500

    lab2 statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,24 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+    if(temperature > 80)
+    {
+        console.log("Watering on")
+    }
+    else{
+        console.log("Watering off")
+    }
+
+    if(timeOfDay == "evening" || timeOfDay == "night")
+    {
+       console.log("Lights on") 
+    }
+    else{
+        console.log("Lights off")
+    }
+while (soilMoisture < 40){
+    console.log("current value:", soilMoisture);
+    soilMoisture += 5;
+    
+}
+    console.log(soilMoisture)
